d80bd0d1110a6537cd5719b19dd3f737363a2961
ScavengerOwl <33069522+ScavengerOwl@users.noreply.github.com>
2017-12-20 18:35:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ async def flip(ctx):
     await ctx.send(embed=emb)
 
 @Scrap.command()
-async def help(self, ctx):
+async def help(ctx):
     """ Shows this message. """
     msg = await ctx.send(embed=Embed(title='Help is coming.', color=discord.Color.dark_magenta()))
     e = Embed(title='Hello there!', color=discord.Color.magenta(), description='Every command should start with the prefix ***$***')
