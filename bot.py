d6a3152cb1e5ca5cfd55b5d992b8b6b560a6eae3
ScavengerOwl <33069522+ScavengerOwl@users.noreply.github.com>
2017-12-20 15:24:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,12 +65,17 @@ async def flip(ctx):
     await ctx.send(embed=emb)
 
 @Scrap.command()
-async def khelp(ctx):
-    """ Helps users with commands """
-    await ctx.send("Every command should start with the prefix ***$***")
-    await ctx.send("```ADMIN:   ping, kick (WORK IN PROGRESS)```")
-    await ctx.send("```Fun:     cookie [@username], flip, PM [message] ```")
-    await ctx.send("```Details: info [@username] ```")
+    async def help(self, ctx):
+        """ Shows this message. """
+        msg = await ctx.send(embed=Embed(title='Help is coming.', color=discord.Color.dark_magenta()))
+        e = Embed(title='Hello there!', color=discord.Color.magenta(),
+                  description='Every command should start with the prefix ***$***')
+        for command_obj in self.bot.all_commands.values():
+            if not command_obj.hidden:
+                e.add_field(name=f'{command_obj.name.title()}',
+                            value=f'{command_obj.help}',
+                            inline=False)
+        await msg.edit(embed=e)
 
 
 Scrap.run(os.getenv("TOKEN"))
